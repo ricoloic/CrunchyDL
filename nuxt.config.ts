ff4622b14aa68fec8c37e24123c2fa9cbc4d4fa4
stratuma <stratumst@proton.me>
2024-06-28 20:25:30 +0200

    fixed hot reload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 export default defineNuxtConfig({
+    vite: {
+        server: {
+            hmr: {
+                port: 3000,
+                clientPort: 3000
+            }
+        }
+    },
     ssr: false,
     modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/google-fonts'],
     googleFonts: {
