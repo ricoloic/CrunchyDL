083f2936d4d1c2ed64c116909d7b4e978c672c4b
stratuma <stratumst@proton.me>
2024-06-30 00:53:36 +0200

    grammar

--- components/Settings/Crunchyroll.vue
+++ components/Settings/Crunchyroll.vue
@@ -41,8 +41,8 @@
         <div class="flex flex-col items-center p-3 bg-[#11111189] rounded-xl select-none">
             <div class="text-sm mb-2">Subtitle Resampler (Experimental)</div>
             <select v-model="selectedSubResampler" @change="setResampler()" class="bg-[#5c5b5b] w-full focus:outline-none px-3 py-2 rounded-xl text-sm text-center cursor-pointer">
-                <option :value="true">Actived</option>
-                <option :value="false">Deactivated</option>
+                <option :value="true">Enabled</option>
+                <option :value="false">Disabled</option>
             </select>
             <div class="text-xs mt-2"> Fixes broken crunchyroll subtitles </div>
         </div>
